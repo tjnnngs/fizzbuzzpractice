f4a200f7a959aa84ede3276f828ee3a49ca92330
TONYA JENNINGS <tjennings@kcs046907-td.kcs.dom>
2017-10-11 10:24:04 -0400

    divisible by 15, 5, 3

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,13 @@ x=0
 
 100.times do
 	x = x + 1
-	if x % 2 == 0
-		p "even"
-	else 
+	if x % 15 == 0
+		p "fizzbuzz"
+	elsif x % 5 == 0
+		p "buzz"
+	elsif x % 3 == 0
+		p "fizz" 
+	else
 		p x
 end
 end
